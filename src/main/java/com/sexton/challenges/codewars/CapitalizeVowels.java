8b5b9d5477f9e99216238f35b65cbd83111ef280
Justin Sexton <justinsexton.dev@gmail.com>
2020-12-08 16:36:35 -0600

    Refactored capitalize vowels solution to make use of regex

--- src/main/java/com/sexton/challenges/codewars/CapitalizeVowels.java
+++ src/main/java/com/sexton/challenges/codewars/CapitalizeVowels.java
@@ -1,28 +1,27 @@
 package com.sexton.challenges.codewars;
 
+import java.util.regex.Pattern;
+
 public final class CapitalizeVowels {
-    public static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
+    // First implementation. Was written roughly 4 years ago.
+    // public static final Set<Character> VOWELS = Set.of('a', 'e', 'i', 'o', 'u');
+    // public static String capitalize(final String string) {
+    // final char[] characters = string.toCharArray();
+    //
+    //     for (int i = 0; i < characters.length; i++) {
+    //         final char character = characters[i];
+    //         final boolean isVowel = VOWELS.contains(character);
+    //         if (isVowel) {
+    //             characters[i] = Character.toUpperCase(character);
+    //         }
+    //     }
+    //
+    //     return new String(characters);
+    // }
 
     public static String capitalize(final String string) {
-        final char[] characters = string.toCharArray();
-
-        for (int i = 0; i < characters.length; i++) {
-            final char character = characters[i];
-            if (isVowel(character)) {
-                characters[i] = Character.toUpperCase(character);
-            }
-        }
-
-        return new String(characters);
-    }
-
-    private static boolean isVowel(final char character) {
-        for (final char vowel : VOWELS) {
-            if (vowel == character) {
-                return true;
-            }
-        }
-
-        return false;
+        return Pattern.compile("[aeiou]")
+                .matcher(string)
+                .replaceAll(match -> match.group().toUpperCase());
     }
 }
--- src/test/java/com/sexton/challenges/codewars/CapitalizeVowelsTests.java
+++ src/test/java/com/sexton/challenges/codewars/CapitalizeVowelsTests.java
@@ -1,14 +1,33 @@
 package com.sexton.challenges.codewars;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CapitalizeVowelsTests {
-    @Test
-    public void testCapitalizeVowels() {
-        assertEquals("", CapitalizeVowels.capitalize(""));
-        assertEquals("HEllOWOrld!", CapitalizeVowels.capitalize("HelloWorld!"));
-        assertEquals("SUndAy", CapitalizeVowels.capitalize("Sunday"));
+    @ParameterizedTest
+    @ArgumentsSource(CapitalizeVowelsArgumentProvider.class)
+    public void testCapitalizeVowels(final String expected, final String input) {
+        final String actual = CapitalizeVowels.capitalize(input);
+        assertEquals(expected, actual);
+    }
+
+    private static class CapitalizeVowelsArgumentProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    arguments("", ""),
+                    arguments("HEllOWOrld!", "HelloWorld!"),
+                    arguments("SUndAy", "Sunday"),
+                    arguments("bbb", "bbb")
+            );
+        }
     }
 }
